d04eebbaea927e70d27a16858865e6e6354d3d9f
ken1714 <96936578+ken1714@users.noreply.github.com>
2024-02-01 21:46:31 +0900

    style: '='の位置を統一

--- env/stg/main.tf
+++ env/stg/main.tf
@@ -82,14 +82,14 @@ module "load_balancing" {
 
 
 module "dns" {
-    depends_on             = [google_project_service.service]
-    source                 = "../../modules/CloudDNS"
-    project_id             = var.project_id
-    region                 = var.region
-    domain                 = var.domain
-    frontend_cloudrun_name = module.frontend.cloudrun_id
-    dns_records_A          = var.dns_records_A
-    dns_records_AAAA       = var.dns_records_AAAA
+    depends_on                 = [google_project_service.service]
+    source                     = "../../modules/CloudDNS"
+    project_id                 = var.project_id
+    region                     = var.region
+    domain                     = var.domain
+    frontend_cloudrun_name     = module.frontend.cloudrun_id
+    dns_records_A              = var.dns_records_A
+    dns_records_AAAA           = var.dns_records_AAAA
     dns_auth_record_name_CNAME = module.certificate_manager.dns_auth_record_name_CNAME
     dns_auth_record_data_CNAME = module.certificate_manager.dns_auth_record_data_CNAME
 }
