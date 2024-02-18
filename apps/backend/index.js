153a6e1bb7f1d0d17c362a2df395355243b06032
ken1714 <96936578+ken1714@users.noreply.github.com>
2024-02-18 18:23:48 +0900

    feat: DB操作をHTTPリクエストで行えるよう修正

--- apps/backend/index.js
+++ apps/backend/index.js
@@ -1,8 +1,6 @@
 console.log('Server Start!');
 
 const {Pool} = require('pg');
-const http = require('http');
-const fs = require('fs');
 const dockerNames = require('docker-names');
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -37,59 +35,53 @@ function getRandomInt(maxValue) {
     return Math.floor(Math.random() * maxValue);
 }
 
-
-// PostgreSQLプールを作成
-const pool = new Pool(dbConfig);
-
-// httpサーバーを作成
-const PORT = 3000;
-const server = http.createServer(async (req, res) => {
-    fs.readFile('./index.html', 'utf-8', async (error, data) => {
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write(data);
-
-        // SQLへ接続
-        try {
-            const client = await pool.connect();
-            res.write('<h2>Connection is succeed.</h2>');
-        } catch(error) {
-            res.write('<h2>Connection is failed.</h2>');
-        }
-
-        // テーブル作成
-        try {
-            await pool.query('BEGIN');
-            await pool.query(createTableQuery);
-            await pool.query('COMMIT');
-            res.write('<h2>Creating the table is succeed.</h2>');
-        } catch(error) {
-            res.write('<h2>Creating the table is failed.</h2>');
-        }
-
-        // レコードの追加
-        try {
-            await pool.query('BEGIN');
-            await pool.query(`INSERT INTO ${TABLE_NAME} (id, name, age) VALUES (${getRandomInt(MAX_ID)}, '${dockerNames.getRandomName()}', ${getRandomInt(MAX_AGE)});`);
-            await pool.query('COMMIT');
-            res.write('<h2>Inserting a record is succeed.</h2>');
-        } catch(error) {
-            res.write('<h2>Inserting a record is failed.</h2>');
-        }
-        res.end();
-    });
-});
-
-server.listen(PORT);
-
 const app = express();
-app.use(bodyParser.urlencoded({ extended: true }))
-app.listen(PORT)
+app.use(bodyParser.urlencoded({ extended: true }));
+app.listen(process.env.PORT);
+
 app.get('/api/get', function (req, res) {
-    res.send({'message': 'Receive a Get request'})
-    console.log('Receive a Get request')
+    res.send({'message': 'Receive a Get request'});
+    console.log('Receive a Get request');
 })
+
 app.post('/api/post', function (req, res) {
-    res.send({'message': 'Receive a POST request as ' + req.body.text})
-    console.log('Receive a POST request')
-    console.log(req.body)
+    res.send({'message': 'Receive a POST request as ' + req.body.text});
+    console.log('Receive a POST request');
+    console.log(req.body);
+})
+
+// TODO: postリクエストに変更すべき
+app.get('/api/db', async (req, res) => {
+    const pool = new Pool(dbConfig);
+
+    let isConnected = false;
+    try {
+        const client = await pool.connect();
+        isConnected = true;
+    } catch(error) {
+        isConnected = false;
+    }
+
+    // テーブル作成
+    let isTableCreated = false;
+    try {
+        await pool.query('BEGIN');
+        await pool.query(createTableQuery);
+        await pool.query('COMMIT');
+        isTableCreated = true;
+    } catch(error) {
+        isTableCreated = false;
+    }
+
+    // レコードの追加
+    let isRecordInserted = false;
+    try {
+        await pool.query('BEGIN');
+        await pool.query(`INSERT INTO ${TABLE_NAME} (id, name, age) VALUES (${getRandomInt(MAX_ID)}, '${dockerNames.getRandomName()}', ${getRandomInt(MAX_AGE)});`);
+        await pool.query('COMMIT');
+        isRecordInserted = true;
+    } catch(error) {
+        isRecordInserted = false;
+    }
+    res.send({isConnected, isTableCreated, isRecordInserted});
 })
